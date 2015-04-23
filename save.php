1ebde2ff0523cef7da98f8e4df9a7032ab17a65a
Christopher Bishop <bishopc@alfredstate.edu>
2015-04-23 00:15:52 -0400

    making sure all files are up to date

--- save.php
+++ save.php
@@ -1,12 +1,11 @@
 <?php
-include 'nav.php';
-$uploaddir = './uploads/';
-$uploadfile = $uploaddir . basename($_FILES['userfile']['name']);
-
-if (move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile)) {
-    echo "File is valid, and was successfully uploaded.\n";
-} else {
+  include 'nav.php';
+  $uploaddir = './uploads/';
+  $uploadfile = $uploaddir . basename($_FILES['userfile']['name']);
+  if (move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile)) {
+    include 'success.php';
+  } else {
     include 'error.php';
-}
-include 'foot.php';
+  }
+  include 'foot.php'; 
 ?>
--- upload.php
+++ upload.php
@@ -2,10 +2,12 @@
 <script src="js/jquery.min.js"></script>
 <script src="js/sanitize.js"></script>
 <div id="error"></div>
-<form class="form-inline" action="save.php" onsubmit="return submitFile()" enctype="multipart/form-data" method="POST">
-  <label for="userfile">Upload a file: </input>
-  <input name="userfile" id="userfile" type="file" />
-  <br />
-  <input type="submit" class="btn btn-primary" id="submit-btn" value="Send File" />
-</form>
+<div id="upload" class="content">
+  <form class="form-inline" action="save.php" onsubmit="return submitFile()" enctype="multipart/form-data" method="POST">
+    <label for="userfile">Upload a file: </input>
+    <input name="userfile" id="userfile" type="file" />
+    <br />
+    <input type="submit" class="btn btn-primary" id="submit-btn" value="Send File" />
+  </form>
+</div>
 <?php include 'foot.php' ?>
